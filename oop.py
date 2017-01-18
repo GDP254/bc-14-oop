571b23e625a897575406cd809c8bf331868e357f
GDP254 <stvnkrs@gmail.com>
2017-01-19 00:19:12 +0300

    Adding demonstration of composition

--- oop.py
+++ oop.py
@@ -1,4 +1,5 @@
-import unittest
+import time
+
 """
 NARRATIVE/REQUIREMENTS
 	An account has two sides to it representing incoming and outgoing flows
@@ -13,79 +14,123 @@ NARRATIVE/REQUIREMENTS
 			cr when decreasing, dr when increasing
 		Income (What one earns)
 			dr when decreasing, cr when increasing
+	A transaction in this case is a change in the state of an account. It needs to show: 
+		when the change occured,
+		what was changed,
+			To know this a transaction will need to know the:
+				type of account being changed,
+				the name of the account 
+		the extent of change
+		the action taken on the account,
+"""
+
+class TransactionStore:
+	_ALL = {}
+
+	def __init__(self):
+		pass
+
+	def save(self, trans):
+		self._ALL[trans.get_time()] = [trans.get_amt(), trans.get_acctype(), trans.get_transtype()]
+		print "Saving Transaction for "+trans.get_acctype()+" at "+trans.get_time()
+
+	def print_all(self):
+		for key, value in self._ALL.iteritems():
+			print key
+			print value
+			print "Time %s : %s | %s | %s" % (str(key), value[0], value[1], str(value[2]))
 
 """
+	Both the Transaction and TransactionStore are use to demonstration COMPOSITION
+	An account makes use of the Transaction object to commit changes
+	The Transaction makes use of the TransactionStore to store the changed
+	The TransactionStore makes use of the Transaction to access when it is required to store
+"""
+class Transaction(object):
+	__when = None
+	__acctype = None #what
+	__transtype = None #what
+	__amt = 0 #extent
+
+	def __init__(self, amt, acctype, transtype):
+		self.__when = time.ctime()
+		self.__amt = amt
+		self.__acctype = acctype
+		self.__transtype = transtype
+		print "Created Transaction for "+self.__acctype+" at "+self.__when
+
+	def commit(self):
+		TransactionStore().save(self)
+		TransactionStore().print_all()
+	
+	def get_time(self):
+		return self.__when
+
+	def get_amt(self):
+		return self.__amt
+
+	def get_acctype(self):
+		return self.__acctype
+
+	def get_transtype(self):
+		return self.__transtype
 
 class Account(object):
 	__balance = 0
 
 	def __init__(self, name):
 		self.name = name
+		print "Account instance created."
 
 	def get_balance(self):
+		print "Current balance: %d queried" % self.__balance
 		return self.__balance
 
 	def set_balance(self, bal):
 		self.__balance = bal
+		print "Current balance set to %d" % bal
+
+	def debit(self, amt):
+		print "Parent debit for "+self.__class__.__name__+" account"
+		Transaction(amt, self.__class__.__name__, 'debit').commit()
+
+	def credit(self, amt):
+		print "Parent credit "+self.__class__.__name__+" account"
+		Transaction(amt, self.__class__.__name__, 'credit').commit()
+
 
 class Asset(Account):
 	def debit(self, amt):
-		self.set_balance(self.get_balance() + amt)
+		Account.debit(self, amt)
+		prevbal = self.get_balance()
+		bal = self.get_balance() + amt
+		print "Asset account: %s Debited %d, Balance changed from %d to %d" % (self.name, amt, prevbal, bal)
+		self.set_balance(bal)
 
 	def credit(self, amt):
-		self.set_balance(self.get_balance() - amt)
+		Account.credit(self, amt)
+		prevbal = self.get_balance()
+		bal = self.get_balance() - amt
+		print "Asset account: %s Credited %d, Balance changed from %d to %d" % (self.name, amt, prevbal, bal)
+		self.set_balance(bal)
 
 class Liability(Account):
 	def debit(self, amt):
-		self.set_balance(self.get_balance() - amt)
+		Account.debit(self, amt)
+		prevbal = self.get_balance()
+		bal = self.get_balance() - amt
+		print "Liability account: %s Debited %d, Balance changed from %d to %d" % (self.name, amt, prevbal, bal)
+		self.set_balance(bal)
 
 	def credit(self, amt):
-		self.set_balance(self.get_balance() + amt)
+		Account.credit(self, amt)
+		prevbal = self.get_balance()
+		bal = self.get_balance() + amt
+		print "Liability account: %s Credited %d, Balance changed from %d to %d" % (self.name, amt, prevbal, bal)
+		self.set_balance(bal)
 
 
-class AccountTest(unittest.TestCase):
-	#The following test case is a demonstration of an OBJECT
-	def test_account_instance_of_object(self):
-		acc = Account('Honda')
-		self.assertIsInstance(acc, object, msg='The object should be an instance of the `object` class')
 
-	#The following test cases are a demonstration of INHERITANCE
-	def test_asset_instance_of_account(self):
-		acc = Asset('Honda')
-		self.assertIsInstance(acc, Account, msg='The object should be an instance of the `Account` class')
-
-	def test_liability_instance_of_account(self):
-		acc = Liability('Honda')
-		self.assertIsInstance(acc, Account, msg='The object should be an instance of the `Account` class')
-
-	def test_asset_instance_of_liability(self):
-		acc = Asset('Honda')
-		self.assertNotIsInstance(acc, Liability, msg='The object should not be an instance of the `Liability` class')
-
-	def test_asset_has_balance(self):
-		acc = Asset('Honda')
-		self.assertEqual(acc.get_balance(), 0, msg='The object should have a balance attribute inherited from Account')
-
-	#The following test cases are a demonstration of ENCAPSULATION
-	def test_balance_access(self):
-		acc = Asset('Honda')
-		with self.assertRaises(AttributeError):
-			acc._balance
-
-	#The following test cases are a demonstration of POLYMORPHISM
-	def test_asset_debit(self):
-		acc = Asset('Honda')
-		acc.debit(600)
-		self.assertEqual(acc.get_balance(), 600, msg='The object should have a positive balance attribute when debit with a balance on 0')
-
-	def test_liability_debit(self):
-		acc = Liability('Honda')
-		acc.credit(600)
-		acc.debit(600)
-		self.assertEqual(acc.get_balance(), 0, msg='The object should have a 0 balance attribute when debited 600 with a balance on 600')
-
-if __name__ == "__main__":
-	unittest.main()
 
 
 
